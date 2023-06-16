f13fbe76db50e0a90b5af50238b0da5c074adc31
Leo Sukharnikov <leonid.sukharnikov@aon.com>
2023-06-16 11:10:08 -0400

    F - WebHandler

--- src/http/handler.rs
+++ src/http/handler.rs
@@ -3,6 +3,7 @@ use super::request::Request;
 use super::status_code::StatusCode;
 use super::request::ParseError;
 use super::method::Method;
+use std::fs;
 
 pub trait Handler {
     fn handle_request(&mut self, request: &Request) -> Response;
@@ -12,16 +13,44 @@ pub trait Handler {
     }
 }
 
-pub struct WebHandler;
+pub struct WebHandler{
+    public_path: String
+}
+
+impl WebHandler {
+    pub fn new(public_path: String) -> Self {
+        Self{ public_path }
+    }
+
+    fn read_file(&self, file_path: &str) -> Option<String>{
+        let path = format!("{}/{}", self.public_path, file_path);
+
+        match fs::canonicalize(path) {
+            Ok(path) => {
+                if path.starts_with(&self.public_path) {
+                    fs::read_to_string(path).ok()
+                }
+                else {
+                    println!("Directory Traversal Attempted!, {}", file_path);
+                    None
+                }
+            }
+            Err(_) => None
+        }
+    }
+}
 
 impl Handler for WebHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
         dbg!(request);
         match request.method() {
             Method::GET => match request.path() {
-                "/" => Response::new(StatusCode::OK, Some("<h1>Welcome</h1>".to_string())),
-                "/hello" => Response::new(StatusCode::OK,Some("<h1>Hello!</h1>".to_string())),
-                _ => Response::new(StatusCode::NotFound, None)
+                "/" => Response::new(StatusCode::OK, self.read_file("index.html")),
+                "/hello" => Response::new(StatusCode::OK,self.read_file("hello.html")),
+                path => match self.read_file(path) {
+                    Some(contents) => Response::new(StatusCode::OK, Some(contents)),
+                    _ =>  Response::new(StatusCode::NotFound, None)
+                }
             },
             _ => Response::new(StatusCode::NotFound, None)
         }
